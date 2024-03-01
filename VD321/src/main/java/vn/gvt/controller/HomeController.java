774548b9814d8dd8999907acb9e055eb8e5e5021
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-03-01 17:33:10 +0700

    update

---
+++ VD321/src/main/java/vn/gvt/controller/HomeController.java
@@ -0,0 +1,35 @@
+package vn.gvt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home/")
+public class HomeController {
+
+    @RequestMapping("index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("about")
+    public String about() {
+        return "about";
+    }
+
+    @RequestMapping("contact")
+    public String contact() {
+        return "/contact";
+    }
+
+    @RequestMapping("feedback")
+    public String feedback() {
+        return "feedback";
+    }
+
+    @RequestMapping("faq")
+    public String faq() {
+        return "faq";
+    }
+}
+
