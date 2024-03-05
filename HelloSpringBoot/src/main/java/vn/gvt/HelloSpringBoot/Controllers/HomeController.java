d41ed341d6ae154c89ca4e53b2158de8f6a4ddbc
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-03-05 16:45:14 +0700

    update Thymeleaf Example

--- HelloSpringBoot/src/main/java/vn/gvt/HelloSpringBoot/Controllers/HomeController.java
+++ HelloSpringBoot/src/main/java/vn/gvt/HelloSpringBoot/Controllers/HomeController.java
@@ -1,12 +1,15 @@
 package vn.gvt.HelloSpringBoot.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
-	public String trangchu() {
+	public String trangchu(ModelMap model) {
+		model.addAttribute("tb","Hello");
+		model.addAttribute("hoten", "Giáp Văn Tài");
 		return "welcome";
 	}
 }
