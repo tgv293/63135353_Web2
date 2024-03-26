9f8e86462ab8c15151deae00f5bccadf111abdcc
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-03-26 16:34:33 +0700

    lần 10

---
+++ giapvantai_QLSV/src/main/java/thiGK/ntu63135353/giapvantai_QLSV/GiapvantaiQlsvApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63135353.giapvantai_QLSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiapvantaiQlsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiapvantaiQlsvApplication.class, args);
+	}
+
+}
---
+++ giapvantai_QLSV/src/test/java/thiGK/ntu63135353/giapvantai_QLSV/GiapvantaiQlsvApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63135353.giapvantai_QLSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiapvantaiQlsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
