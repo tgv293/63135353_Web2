a734dde8dd48e6d9ffe6e655597b84873e0f8d8c
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-03-26 16:58:04 +0700

    lần 12

--- giapvantai_QLSinhVien/src/main/java/thiGK/ntu63135353/giapvantai_QLSinhVien/controllers/SinhVienController.java
+++ giapvantai_QLSinhVien/src/main/java/thiGK/ntu63135353/giapvantai_QLSinhVien/controllers/SinhVienController.java
@@ -1,8 +1,13 @@
 package thiGK.ntu63135353.giapvantai_QLSinhVien.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import thiGK.ntu63135353.giapvantai_QLSinhVien.models.SinhVien;
 
 @Controller
 public class SinhVienController {
@@ -10,6 +15,31 @@ public class SinhVienController {
 	@GetMapping("/home")
 	public String Home() {
 		return "index";
+	}
+	
+
+	static List<SinhVien> dsSinhViens = new ArrayList<SinhVien>();
+
+	static {
+		dsSinhViens.add(new SinhVien("0001", "Mai Cường Thọ",9));
+		dsSinhViens.add(new SinhVien("0002", "Trần Văn Long",8));
+		dsSinhViens.add(new SinhVien("0003", "Nguyễn Thị Hương",7));
+		dsSinhViens.add(new SinhVien("0004", "Lê Anh Dũng",8));
+		dsSinhViens.add(new SinhVien("0005", "Phạm Thị Bích",5));
+		dsSinhViens.add(new SinhVien("0006", "Vũ Minh Hiếu",9));
+		dsSinhViens.add(new SinhVien("0007", "Ngô Thị Lan",8));
+		dsSinhViens.add(new SinhVien("0008", "Đỗ Văn Tùng",6));
+		dsSinhViens.add(new SinhVien("0009", "Hoàng Thị Mai",5));
+		dsSinhViens.add(new SinhVien("0010", "Trần Minh Quân",4));
+		dsSinhViens.add(new SinhVien("0011", "Nguyễn Thành Nam",3));
+		dsSinhViens.add(new SinhVien("0012", "Lý Thị Hà",2));
+
+	}
+	
+	@GetMapping("/danhsachSV")
+	public String listStudent(ModelMap mm) {
+		mm.addAttribute("dsSV", dsSinhViens);
+		return "QLSV";
 	}
 
 }
--- giapvantai_QLSinhVien/src/test/java/thiGK/ntu63135353/giapvantai_QLSinhVien/services/SinhVienServiceImpl.java
+++ giapvantai_QLSinhVien/src/test/java/thiGK/ntu63135353/giapvantai_QLSinhVien/services/SinhVienServiceImpl.java
@@ -1,43 +1,54 @@
-/*
- * package thiGK.ntu63135353.giapvantai_QLSinhVien.services;
- * 
- * import org.springframework.stereotype.Service;
- * 
- * import thiGK.ntu63135353.giapvantai_QLSinhVien.models.SinhVien;
- * 
- * import java.util.ArrayList; import java.util.Collections; import
- * java.util.List;
- * 
- * import org.springframework.data.domain.Page; import
- * org.springframework.data.domain.PageImpl; import
- * org.springframework.data.domain.PageRequest; import
- * org.springframework.data.domain.Pageable;
- * 
- * @Service public class SinhVienServiceImpl implements SinhvienService {
- * 
- * static List<SinhVien> dsSinhVien = new ArrayList<>(); static {
- * dsSinhVien.add(new SinhVien("0001", "Nguyễn Thị An", 0)); dsSinhVien.add(new
- * SinhVien("0002", "Lê Văn Bình", 0)); dsSinhVien.add(new SinhVien("0003",
- * "Trần Thị Cẩm", 0)); dsSinhVien.add(new SinhVien("0004", "Đỗ Văn Dương", 0));
- * dsSinhVien.add(new SinhVien("0005", "Phạm Thị Mai", 0)); dsSinhVien.add(new
- * SinhVien("0006", "Nguyễn Văn Nam", 0)); dsSinhVien.add(new SinhVien("0007",
- * "Lê Thị Oanh", 0)); dsSinhVien.add(new SinhVien("0008", "Nguyễn Văn Phúc",
- * 0)); dsSinhVien.add(new SinhVien("0009", "Trần Thị Quỳnh", 0));
- * dsSinhVien.add(new SinhVien("0010", "Vũ Văn Sơn", 0)); dsSinhVien.add(new
- * SinhVien("0011", "Lê Thị Thanh", 0)); dsSinhVien.add(new SinhVien("0012",
- * "Phạm Văn Tùng", 0)); }
- * 
- * // ----------hết phần hard-code dữ liệu ---------------------
- * 
- * @Override public Page<SinhVien> findPaginated(Pageable pageable) { int
- * pageSize = pageable.getPageSize(); int currentPage =
- * pageable.getPageNumber(); int startItem = currentPage * pageSize;
- * List<SinhVien> list;
- * 
- * if (dsSinhVien.size() < startItem) { list = Collections.emptyList(); } else {
- * int toIndex = Math.min(startItem + pageSize, dsSinhVien.size()); list =
- * dsSinhVien.subList(startItem, toIndex); } return new PageImpl<>(list,
- * PageRequest.of(currentPage, pageSize), dsSinhVien.size()); }
- * 
- * }
- */
+
+package thiGK.ntu63135353.giapvantai_QLSinhVien.services;
+
+import org.springframework.stereotype.Service;
+
+import thiGK.ntu63135353.giapvantai_QLSinhVien.models.SinhVien;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@Service
+public class SinhVienServiceImpl implements SinhvienService {
+
+	static List<SinhVien> dsSinhVien = new ArrayList<>();
+	static {
+		dsSinhVien.add(new SinhVien("0001", "Nguyễn Thị An", 0));
+		dsSinhVien.add(new SinhVien("0002", "Lê Văn Bình", 0));
+		dsSinhVien.add(new SinhVien("0003", "Trần Thị Cẩm", 0));
+		dsSinhVien.add(new SinhVien("0004", "Đỗ Văn Dương", 0));
+		dsSinhVien.add(new SinhVien("0005", "Phạm Thị Mai", 0));
+		dsSinhVien.add(new SinhVien("0006", "Nguyễn Văn Nam", 0));
+		dsSinhVien.add(new SinhVien("0007", "Lê Thị Oanh", 0));
+		dsSinhVien.add(new SinhVien("0008", "Nguyễn Văn Phúc", 0));
+		dsSinhVien.add(new SinhVien("0009", "Trần Thị Quỳnh", 0));
+		dsSinhVien.add(new SinhVien("0010", "Vũ Văn Sơn", 0));
+		dsSinhVien.add(new SinhVien("0011", "Lê Thị Thanh", 0));
+		dsSinhVien.add(new SinhVien("0012", "Phạm Văn Tùng", 0));
+	}
+
+	// ----------hết phần hard-code dữ liệu ---------------------
+
+	@Override
+	public Page<SinhVien> findPaginated(Pageable pageable) {
+		int pageSize = pageable.getPageSize();
+		int currentPage = pageable.getPageNumber();
+		int startItem = currentPage * pageSize;
+		List<SinhVien> list;
+
+		if (dsSinhVien.size() < startItem) {
+			list = Collections.emptyList();
+		} else {
+			int toIndex = Math.min(startItem + pageSize, dsSinhVien.size());
+			list = dsSinhVien.subList(startItem, toIndex);
+		}
+		return new PageImpl<>(list, PageRequest.of(currentPage, pageSize), dsSinhVien.size());
+	}
+
+}
