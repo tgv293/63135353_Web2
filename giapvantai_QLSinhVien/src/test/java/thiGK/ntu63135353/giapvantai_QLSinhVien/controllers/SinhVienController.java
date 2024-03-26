214002287da1c87916d34e06c705619532e8c828
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-03-26 15:13:27 +0700

    lần 2

---
+++ giapvantai_QLSinhVien/src/test/java/thiGK/ntu63135353/giapvantai_QLSinhVien/controllers/SinhVienController.java
@@ -0,0 +1,12 @@
+package thiGK.ntu63135353.giapvantai_QLSinhVien.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SinhVienController {
+	@GetMapping("/home")
+	public String Home() {
+		return "index";
+	}
+}
