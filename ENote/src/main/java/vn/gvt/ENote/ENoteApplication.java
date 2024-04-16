428290eed9de45b5b4edb1c8d55a3a3155309c24
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-04-16 15:51:48 +0700

    push

---
+++ ENote/src/main/java/vn/gvt/ENote/ENoteApplication.java
@@ -0,0 +1,13 @@
+package vn.gvt.ENote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENoteApplication.class, args);
+	}
+
+}
---
+++ ENote/src/test/java/vn/gvt/ENote/ENoteApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.gvt.ENote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
