b78aaabc0f0e7aa4afa71ff9cb0c1604207a615a
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-04-18 22:39:26 +0700

    update ui

--- ENote/src/main/java/vn/gvt/ENote/Controllers/HomeController.java
+++ ENote/src/main/java/vn/gvt/ENote/Controllers/HomeController.java
@@ -9,6 +9,16 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	@GetMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
 
 	@GetMapping("/register")
 	public String register() {
