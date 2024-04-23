9b45c0e3ecf8244712e9675e02bf7bea4b2ad405
tgv293 <102977775+tgv293@users.noreply.github.com>
2024-04-23 16:14:24 +0700

    update

--- ENote/src/main/java/vn/gvt/ENote/Models/User.java
+++ ENote/src/main/java/vn/gvt/ENote/Models/User.java
@@ -9,7 +9,6 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 
 import jakarta.persistence.Entity;
